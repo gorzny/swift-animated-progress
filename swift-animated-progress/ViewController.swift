cdede537b8c7dbbfe03d7d4627bd680e267bacd2
Andrew Gorzny <andrew.gorzny@gmail.com>
2017-12-11 18:45:48 -0500

    Initial Commit

---
+++ swift-animated-progress/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-animated-progress
+//
+//  Created by Andrew Gorzny on 2017-12-11.
+//  Copyright © 2017 Andrew Gorzny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
