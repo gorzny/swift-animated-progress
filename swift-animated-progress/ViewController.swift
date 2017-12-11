d92ce43fc5837c43b3ec61064970e4b55cbb2564
Andrew Gorzny <andrew.gorzny@gmail.com>
2017-12-11 18:51:00 -0500

    Basic circle and outline

--- swift-animated-progress/ViewController.swift
+++ swift-animated-progress/ViewController.swift
@@ -13,6 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Draw Circle
+        
+        let shapeLayer = CAShapeLayer()
+        
+        let center = view.center
+        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        shapeLayer.path = circularPath.cgPath
+        
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        shapeLayer.lineWidth = 10
+        
+        
+        
+        view.layer.addSublayer(shapeLayer)
+        
     }
 
     override func didReceiveMemoryWarning() {
